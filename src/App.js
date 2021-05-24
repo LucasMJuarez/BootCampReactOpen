ffaae983c52c4bcb30f8ec4fc7285a35546edfbf
LucasMJuarez <lumas89@gmail.com>
2021-05-24 19:31:54 -0300

    addNote with forms and onSubmit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React from 'react'
-import Note from './components/Note'
+import React, { useState } from "react";
+import Note from "./components/Note";
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
+
+  const addNote = (e) => {
+    e.preventDefault();
+    const noteOdject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    };
+    setNotes(notes.concat(noteOdject));
+    setNewNote("");
+  };
+
+  const handlerChange = (e) => {
+    console.log(e.target.value);
+    setNewNote(e.target.value);
+  };
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map((note) => 
+        {notes.map((note) => (
           <Note key={note.id} note={note} />
-        )}
+        ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handlerChange} />
+        <button type="submit"> save </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
