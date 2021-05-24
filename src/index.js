48d63f0c3547d193ce0e64b90bd5d6a0e2a2ffcd
LucasMJuarez <lumas89@gmail.com>
2021-05-23 23:39:34 -0300

    handlers matrix states

--- src/index.js
+++ src/index.js
@@ -1,32 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const Hello = (
-  {name, age}
-) => {
-  //destructuring
-
-  const bornYear = () => new Date().getFullYear() - age;
-  //calcula el ano de nacimiento
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  );
-};
-
 const App = () => {
-  const name = "Peter";
-  const age = 10;
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+  };
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 7} />
-      <Hello name={name} age={age} />
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(" ")}</p>
     </div>
   );
 };
