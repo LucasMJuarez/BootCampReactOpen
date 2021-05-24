d40616a08747686c7e9d55c8ef8dcf1fbf6591a2
LucasMJuarez <lumas89@gmail.com>
2021-05-24 20:47:23 -0300

    added handler catch errors

--- src/services/notes.js
+++ src/services/notes.js
@@ -21,7 +21,7 @@ const update = (id, newObject) => {
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  getAll: getAll,
-  create: create,
-  update: update,
+  getAll,
+  create,
+  update,
 };
